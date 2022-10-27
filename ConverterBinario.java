69d8d5ab63111d5c16c713fe7aa5266bf665ef89
Nicolas <nicolas.w3nder@gmail.com>
2022-10-26 23:30:18 -0300

    Adicionando projeto ConverterBinario

---
+++ ConverterBinario.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+public class ConverterBinario {
+  public static void main(String[] args) {
+    int resultado =0;
+   
+    String binario = JOptionPane.showInputDialog(null, "Digite o numero em binario: ");
+    char[] numBinario = binario.toCharArray();
+    
+
+    for (int i = 0; i < numBinario.length; i++) {
+      resultado += (Character.getNumericValue(numBinario[i]))*(Math.pow(2, i)) ;
+    }
+    JOptionPane.showMessageDialog(null, "Valor em decimal: " + resultado);
+    System.out.println(resultado);
+
+  }
+}
