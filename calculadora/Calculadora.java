c7e49384d5dd318cc9d3f5edf8d2a2e504e5f8c4
Nicolas <nicolas.w3nder@gmail.com>
2022-11-02 02:19:33 -0300

    colocando resultado como private e mudando variavel operador

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -5,13 +5,16 @@
 
 
 
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Calculadora {
   private List<String> numeros = new ArrayList<String>();
-  Double resultado;
-  private String operador;
+  private String resultado;
+  private List<String> operador = new ArrayList<String>();
 
   public void setNumeros( String numero){
     this.numeros.add(numero);
@@ -19,36 +22,44 @@ public class Calculadora {
   }
 
   public void setOperador(String operadorBotao){
-    this.operador = operadorBotao;
+    this.operador.add(operadorBotao);
   }
 
-  private void verificar(List<String> numeros, String operador){
+  private void setResultado(Double resultadoCalc){
+    this.resultado = String.valueOf(resultadoCalc);
+  }
+
+  public String getResultado(){
+    return resultado;
+  }
+
+  private void verificar(List<String> numeros, List<String> operador){
     if(numeros.size() < 2 ){
       return;
     }else{
       calcular(numeros, operador);
-      
+      numeros.set(0, resultado);
+
     }
 
 
   }
 
-  private Double calcular( List<String> numeros, String operador){
+  private void calcular( List<String> numeros, List<String>operador){
     
     double numero1 = Double.parseDouble(numeros.get(1));
     double numero2 = Double.parseDouble(numeros.get(2));
     
-    switch (operador){
+    switch (operador.get(0)){
       case "+": 
-        return resultado = numero1 + numero2;
+        setResultado(numero1 + numero2);
       case "-": 
-        return resultado = numero1 - numero2;
+        setResultado(numero1 - numero2);
       case "/": 
-        return resultado = numero1 / numero2;
+        setResultado(numero1 / numero2);
       case "*": 
-        return resultado = numero1 * numero2; 
+        setResultado(numero1 * numero2); 
     }
-    return numero2;
   
   }
 
