57d8643d0b70ecf4458377c4d7bfccd4f9d1cd9c
Nicolas <nicolas.w3nder@gmail.com>
2022-11-04 00:12:18 -0300

    adicionando botões e corrigindo bugs do jogoDaVelha

--- jogoDaVelha.java
+++ jogoDaVelha.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 
 
 public class jogoDaVelha extends JFrame{
+  static String jogada = "X";
   JButton [] bt = new JButton[9];
   
   public jogoDaVelha(){
@@ -13,8 +14,8 @@ public class jogoDaVelha extends JFrame{
     setDefaultCloseOperation(3);
     setLayout(null);
     setBounds(250,100,420,500);
-    int cont =0;
-    String jogada = "X";
+    int cont = 0;
+   
     boolean click[] = new boolean[9];
 
     for(int i=0;i<3;i++){
@@ -36,19 +37,92 @@ public class jogoDaVelha extends JFrame{
         click[0] = true;
       }
     });
-
+    bt[1].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[1] == false){
+          mudarJogada(jogada, bt[1]);
+        }
+        click[1] = true;
+      }
+    });
+    bt[2].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[2] == false){
+          mudarJogada(jogada, bt[2]);
+        }
+        click[2] = true;
+      }
+    });
+    bt[3].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[3] == false){
+          mudarJogada(jogada, bt[3]);
+        }
+        click[3] = true;
+      }
+    });
+    bt[4].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[4] == false){
+          mudarJogada(jogada, bt[4]);
+        }
+        click[4] = true;
+      }
+    });
+    bt[5].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[5] == false){
+          mudarJogada(jogada, bt[5]);
+        }
+        click[5] = true;
+      }
+    });
+    bt[6].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[6] == false){
+          mudarJogada(jogada, bt[6]);
+        }
+        click[6] = true;
+      }
+    });
+    bt[7].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[7] == false){
+          mudarJogada(jogada, bt[7]);
+        }
+        click[7] = true;
+      }
+    });
+    bt[8].addActionListener(new java.awt.event.ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent ae ){
+        if(click[8] == false){
+          mudarJogada(jogada, bt[8]);
+        }
+        click[8] = true;
+      }
+    });
+  
   }
 
   public static void mudarJogada(String jogada, JButton bt){
     if(jogada=="X"){
       bt.setText(jogada);
-      jogada="O";
+      jogoDaVelha.jogada="O";
     }else{
       bt.setText(jogada);
-      jogada="X";
+      jogoDaVelha.jogada="X";
     }
   }
 
+  
   public static void main (String[] args) {
     new jogoDaVelha();  
   }
